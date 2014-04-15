427932e674743e478f7a3c60db7e2e5a012d40f4
Richijs <rihardsbuls@gmail.com>
2014-04-15 16:25:07 +0300

    still , stuff can be injected

--- app/views/users/profile.blade.php
+++ app/views/users/profile.blade.php
@@ -1,6 +1,6 @@
 @extends("layout")
 @section("content")
-    <h2>Hello, {{ Auth::user()->username }}</h2>
+    <h2>Hello, {{{ Auth::user()->username }}}</h2>
     <p>Your profile page.</p>
     
     <a href="{{{ URL::to("/editUser/".Auth::user()->id) }}}">Edit UserData</a>
