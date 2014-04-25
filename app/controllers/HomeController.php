eb11c237015fad391410b30c0a445e88f43b4de0
Richijs <rihardsbuls@gmail.com>
2014-04-25 15:05:45 +0300

    top 5 vacancies at HOME

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -8,6 +8,19 @@ class HomeController extends BaseController {
         
     public function viewHome()
     {
-        return View::make("home");
+        
+        $vacancies = Vacancie::all();
+        foreach ($vacancies as $vacancie)
+        {
+            $vacancie->applied = Application::where('vacancie_id',$vacancie->id)->count();
+            
+            $creator = User::where('id',$vacancie->creator_id)->first();
+            $vacancie->creatorName = $creator->username;
+        }
+        //pirmās piecas top vakances  (Pēc visvairāk pieteikušo skaita)
+        $topVacancies = $vacancies->sortBy('applied')->reverse()->take(5);
+        
+        
+        return View::make("home",array('topVacancies'=>$topVacancies));
     }
 }
--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -5,4 +5,27 @@
     <div>āē  strādā :)</div>
     {{trans('validation.required')}} <-- test <!-- translation example -->
     
+    <h3>top 5 vacancies</h3>
+    @foreach ($topVacancies as $vacancie)
+    <div>
+        <a href="/viewVacancie/{{{$vacancie->id}}}">{{{ $vacancie->name }}}</a>
+        <span><b>created at:</b> {{{ date('d.m.y H:i',strtotime($vacancie->created_at)) }}}</span>
+                
+                @if ($vacancie->poster)
+                <span>
+                    <img src="{{URL::to('/')}}/{{{$vacancie->poster}}}" width="50" height="50" alt="vacancie poster"/>
+                </span>
+                @endif
+                
+        <span>
+                    <b> Added by: </b>
+                    <a href="/viewUser/{{{$vacancie->creator_id}}}">{{{ $vacancie->creatorName }}}</a>
+        </span>
+        <span>
+                    <b>___Applied for this Vacancie:</b> {{{$vacancie->applied}}}
+        </span>
+    </div> 
+    @endforeach
+    
+    
 @stop
