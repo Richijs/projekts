714e6791e49bc4eedbc2389771164a51ba36cdc4
Richijs <rihardsbuls@gmail.com>
2014-05-08 14:12:58 +0300

    request user pass change bootstr form

--- app/views/users/request.blade.php
+++ app/views/users/request.blade.php
@@ -4,18 +4,36 @@
 
     {{ Form::open([
         "route"        => "users/request",
-        "autocomplete" => "off"
+        "autocomplete" => "off",
+        "class"        => "form-horizontal",
+        "role"         => "form"
     ]) }}
-        {{ Form::label("email", "Email") }}
-        {{ Form::text("email", Input::get("email"), [
-            "placeholder" => "email"
+    
+    <div class="form-group @if ($errors->first('email')) has-error@endif">
+        {{ Form::label("email", "Email",[
+            "class"  => "col-sm-4 control-label"
         ]) }}
+        
+        <div class="col-sm-4">
+            {{ Form::text("email", Input::get("email"), [
+                "placeholder" => "email",
+                "class"       => "form-control"
+            ]) }}
+        </div>
+    
         @if ($error = $errors->first("email"))
-            <div class="error">
+            <div class="error col-sm-offset-4 col-sm-4">
                 {{ $error }}
             </div>
         @endif
-        {{ Form::submit("reset") }}
+    </div>
+    
+        <div class="form-group">
+            <div class="col-sm-offset-4 col-sm-4">
+                {{ Form::submit("request",["class" => "btn btn-warning btn-block"]) }}
+            </div>
+        </div>
+        
     {{ Form::close() }}
 @stop
 @section("footer")
