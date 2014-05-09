b8e9d2bd1d1b1c2ffb5c831146046d72fb7b208c
Richijs <rihardsbuls@gmail.com>
2014-05-09 10:55:13 +0300

    design stuff

--- app/views/header.blade.php
+++ app/views/header.blade.php
@@ -2,79 +2,95 @@
     <div class="header">
         <div class="container">
             <h1>VakancesLV</h1>
-            <nav>
-                <a href="{{ URL::route("home") }}">
-                    home
-                </a>
-                |
+            <nav class="navbar navbar-inverse" role="navigation">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="{{ URL::route("home") }}">home</a
+                    </li>
+                    <li>
                 <a href="{{ URL::route("users/viewAllUsers") }}">
                     ViewAllUsers
                 </a>
-                |
+                    </li>
+                    <li>
                 <a href="{{ URL::route("vacancies/viewAllVacancies") }}">
                     ViewAllVacancies
                 </a>
-                |
+                    </li>
+                    <li>
                 <a href="{{ URL::route("seekers/viewAllSeekers") }}">
                     ViewAllJobSeekers
                 </a>
-                |
+                </li>
+                    <li>
                 <a href="{{ URL::to("/about") }}">
                     About
                 </a>
-                |
+                </li>
+                    <li>
                 <a href="{{ URL::route("messaging/contact") }}">
                     Contact Admin
                 </a>
-                |
+                    </li>
             @if (Auth::check())
-                Logged in as:
+                <li>
+                    <span class="loggedAs">Logged in as:</span>
                 <a href="{{ URL::to("/viewUser/".Auth::user()->id) }}">
                     {{{Auth::user()->username}}}
                 </a>
-                |
+                </li>
+                    <li>
                 <a href="{{ URL::route("users/profile") }}">
                     My profile
                 </a>
-                |
+                </li>
+                    <li>
                 <a href="{{ URL::route("users/logout") }}">
                     logout
                 </a>
-                |
+                    </li>
                 @if (Auth::user()->userGroup===1 || Auth::user()->userGroup===2)
+                <li>
                     <a href="{{ URL::route("vacancies/add") }}">
                         Add Vacancie
                     </a>
-                    |
+                </li>
                 @endif
                 @if (Auth::user()->userGroup===1 || Auth::user()->userGroup===3)
-                    <a href="{{ URL::route("seekers/add") }}">
+                <li>    
+                <a href="{{ URL::route("seekers/add") }}">
                         Add JobSeeker data
                     </a>
-                    |
+                </li>
                 @endif
             @else
+            <li>
                 <a href="{{ URL::route("users/login") }}">
                     login
                 </a>
-                |
+                </li>
+                    <li>
                 <a href="{{ URL::route("users/register") }}">
                     register
                 </a>
+                    </li>
             @endif
-            </nav>
-            <div class="lang">       
-                <div class="btn-group btn-toggle">
-                    <a href="{{ URL::to("/lang/lv") }}" class="btn btn-xs btn-default
-                        @if (Config::get('app.locale')=='lv') btn-primary active @endif">
-                        LV
-                    </a>
-                    <a href="{{ URL::to("/lang/en") }}" class="btn btn-xs btn-default
-                        @if (Config::get('app.locale')=='en') btn-primary active @endif">
-                        EN
-                    </a>
+                </ul>
+                
+                <div class="lang">       
+                    <div class="btn-group btn-toggle">
+                        <a href="{{ URL::to("/lang/lv") }}" class="btn btn-xs btn-default
+                            @if (Config::get('app.locale')=='lv') btn-primary active @endif">
+                            LV
+                        </a>
+                        <a href="{{ URL::to("/lang/en") }}" class="btn btn-xs btn-default
+                            @if (Config::get('app.locale')=='en') btn-primary active @endif">
+                            EN
+                        </a>
+                    </div>
                 </div>
-            </div>
+                
+            </nav>
         </div>
     </div>
 @show
