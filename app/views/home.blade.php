1eccfcf3f67bdf038abb17cd12e11393647214eb
Richijs <rihardsbuls@gmail.com>
2014-05-10 10:04:08 +0300

    some tests , home page

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -8,6 +8,57 @@
         {{trans('validation.required')}} <-- test <!-- translation example -->
     </div>
 
+
+
+
+
+
+<div class="container-fluid">
+    
+    <div class="row">
+        <div id="carousel-example-generic" class="carousel slide col-sm-6 col-sm-offset-2" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="{{URL::to('/')}}/uploads/vacanciePosters/default.jpeg" alt="...">
+      <div class="carousel-caption">
+          <h4>smth smth</h4>
+          <p>yo yo yo</p>
+      </div>
+    </div>
+    <div class="item">
+      <img src="{{URL::to('/')}}/uploads/vacanciePosters/default.jpeg" alt="...">
+      <div class="carousel-caption">
+          <h4>smth smth</h4>
+          <a href="#"> saggs </a>
+          <p>yo yo yo</p>
+      </div>
+    </div>
+
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+    <!--<span class="glyphicon glyphicon-chevron-left"></span>-->
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+    <!--<span class="glyphicon glyphicon-chevron-right"></span>-->
+  </a>
+</div>
+  
+    </div>
+    
+    
+    <div class="row">
+        
+        <div class="col-sm-7">
     <h3>top 5 vacancies</h3>
     @foreach ($topVacancies as $vacancie)
     <div>
@@ -31,9 +82,11 @@
 
     </div> 
     @endforeach
+        </div>
     
+        
+        <div class="col-sm-5">
     <h3>top 5 employers</h3>
-    
     @foreach ($topEmployers as $employer)
     <div>
         @if ($employer->picture)
@@ -48,6 +101,17 @@
         
     </div>
     @endforeach
+        </div>
+    
+    
+    
+    </div>
+    
+</div>
+    
+
+
+    
     
     
 @stop
