b0952677bd088f1b2c33d0ef4bde744659af946a
Richijs <rihardsbuls@gmail.com>
2014-05-13 13:00:03 +0300

    must choose view all vacancie style

--- app/views/vacancies/viewAllVacancies.blade.php
+++ app/views/vacancies/viewAllVacancies.blade.php
@@ -56,7 +56,58 @@
                     
                 </div>
             </div>
+            
+            
+            
+            <div class='panel panel-default'>
+                <div class='panel-heading'>
+                    <a href="/viewVacancie/{{{$vacancie->id}}}">{{{ $vacancie->name }}}</a>
+                </div>
+                <div class='panel-body'>
+                    @if ($vacancie->poster)
+                    <span class="pull-left">
+                        <img src="{{URL::to('/')}}/{{{$vacancie->poster}}}" width="64" height="64" alt="vacancie poster"/>
+                    </span>
+                    @else
+                    <span class="pull-left">
+                        <img src="{{URL::to('/')}}/uploads/vacanciePosters/default.jpeg" width="64" height="64" alt="vacancie poster"/>
+                    </span>
+                    @endif
+                    <!-- else - shows default vacancie pic -->
+                    
+                    <span>                   
+                        <b>created at:</b> {{{ date('d.m.y H:i',strtotime($vacancie->created_at)) }}}
+                    
+                        <b>Company:</b> {{{$vacancie->company}}}   
+                    </span>
+                    
+                    
+                        <b> Added by: </b>
+                        <a href="/viewUser/{{{$vacancie->creator_id}}}">{{{ $vacancie->creatorName }}}</a>
+                        <a href="/viewRecommenders/{{{$vacancie->creator_id}}}">({{{$vacancie->userRecommends}}})</a>
+                    
+                    @if (Auth::check() && $vacancie->creator_id!=Auth::user()->id)
+                    
+                        <a class="btn btn-default" href="/recommend/{{{$vacancie->creator_id}}}">
+                            @if ($vacancie->recommended)
+                                <span class="glyphicon glyphicon-remove-circle"></span>
+                                <span class="glyphicon glyphicon-thumbs-up"></span>
+                            @else
+                                <span class="glyphicon glyphicon-thumbs-up"></span>
+                            @endif
+                        </a>
+                   
+                    @endif
+                        
+                    
+                        <b>Applied for this Vacancie:</b> {{{$vacancie->applied}}}
+                   
+                    
+                </div>
+            </div>
+            
             @endforeach
+            
             
             
             <div>
