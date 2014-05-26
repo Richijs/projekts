f3e1773c6eb15d7d4683774be06f88b2693fe101
Richijs <rihardsbuls@gmail.com>
2014-05-26 15:07:45 +0300

    tr

---
+++ app/lang/lv/buttons.php
@@ -0,0 +1,51 @@
+<?php 
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Titles Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are used by the main titles all across
+        | the application.
+	|
+	*/
+    
+        'to-vacancie' => 'uz Vakanci',
+        'all-vacancies' => 'Visas Vakances',
+        'my-applications' => 'Mani Pieteikumi',
+        'to-application' => 'uz Pieteikumu',
+        'profile' => 'Profils',
+        'edit-application' => 'Rediģēt Pieteikumu',
+        'delete-application' => 'Dzēst pieteikumu',
+        'edit-jobseek-data' => 'Rediģēt darba meklētāja datus',
+        'delete-jobseek-data' => 'Dzēst darba meklētāja datus',
+        'download-cv' => 'Lejupielādēt CV',
+        'my-vacancies' => 'Manas Vakances',
+        'view-your-application' => 'Skatīt Jūsu Pieteikumu',
+        'all-site-users' => 'Visi vietnes lietotāji',
+        'to-my-messages' => 'Uz Manām Ziņām',
+        'reply' => 'Atbildēt',
+        'delete-message' => 'Dzēst Ziņu',
+        'recommenders' => 'Rekomendētāji',
+        'recommendations' => 'Rekomendācijas',
+        'my-profile' => 'Mans Profils',
+        'all-job-seekers' => 'Visi darba meklētāji',
+        'to-jobseek' => 'Uz darba meklējumu',
+        'my-jobseek' => 'Mans darba meklējums',
+        'delete-profile' => 'Dzēst Profilu',
+        'edit-profile' => 'Rediģēt Profilu',
+        'my-messages' => 'Manas Ziņas',
+        'users-who-i-recommended' => 'Lietotāji, kurus esmu rekomendējis',
+        'users-who-recommended-me' => 'Lietotāji, kuri rekomendēja Mani',
+        'request' => 'Pieprasīt',
+        'reset-password' => 'Atiestatīt Paroli',
+        'is-searching-for-job' => 'meklē darbu',
+        'edit-vacancie' => 'Rediģēt Vakanci',
+        'apply-vacancie' => 'Dzēst Vakanci',
+    
+        
+    
+    
+);
---
+++ app/lang/lv/emails.php
@@ -0,0 +1,28 @@
+<?php 
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Titles Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are used by the main titles all across
+        | the application.
+	|
+	*/
+    
+        'registration-at-vacances-was-successfull' => 'Reģistrācija Vakanču vietnē noritēja veiksmīgi',
+        'thanks-for-using-site' => 'Paldies, ka lietojat mūsu vietni.',
+        'new-message' => 'Jauna Vakances ziņa no lietotāja',
+        'reply' => 'Atbildēt',
+        'activate-account' => 'Aktivizēt Jūsu profilu',
+        'to-activate-account' => 'Lai aktivizētu Vakanču vietnes profilu - apmkeklējiet sekojošo saiti',
+        'account-activation' => 'Profila aktivizācija',
+        'if-didnt-request' => 'Ja Jūs nepieprasījāt šo darbību - ignorējiet šo e-pastu.',
+        'to-reset-password' => 'Lai atiestatītu Vakanču vietnes profila paroli - aizpildiet sekojošo formu',
+        'reset-my-password' => 'Atiestatīt manu paroli',
+        'password-reset' => 'Paroles atiestatīšana',
+    
+    
+);
